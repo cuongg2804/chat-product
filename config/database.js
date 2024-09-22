0499d75738d15a515ce7f592540d2c6803e4b001
cuongg2804 <mm2cuong@gmail.com>
2024-09-22 23:35:40 +0700

    four

--- config/database.js
+++ config/database.js
@@ -2,7 +2,12 @@ const mongoose = require("mongoose");
 
 module.exports.connect = async () => {
     try {
-       await mongoose.connect (process.env.MONGOOSE_ULR);
+       await mongoose.connect (process.env.MONGOOSE_ULR,{
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        serverSelectionTimeoutMS: 50000, // Tăng thời gian chờ để kết nối thành công
+        socketTimeoutMS: 45000 // Thời gian chờ cho kết nối socket
+       });
        console.log("Kết nối Database thành công !")
     } catch (error) {
         console.log("Kết nối Database thất bại !")
